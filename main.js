cca40d2d71c23708a8c247a0cf0337704630704f
jisupark0106 <53424105+jisupark0106@users.noreply.github.com>
2020-06-22 19:16:39 +0900

    JS : Scroll Section
    Navbar 메뉴 버튼 클릭시 - 해당 Section으로 Scroll 이동

--- main.js
+++ main.js
@@ -3,10 +3,36 @@ const navbar = document.getElementById(`navbar`);
 const navbarHeight = navbar.offsetHeight;
 
 // navbar top일 때 bgcolor = transparent
-document.addEventListener(`scroll`, function () {
+document.addEventListener(`scroll`, () => {
   if (window.scrollY < navbarHeight) {
     navbar.classList.remove(`scroll--white`);
   } else {
     navbar.classList.add(`scroll--white`);
   }
+});
+
+// navbar menu -- section 이동
+
+//use data-set
+const navbarMenu = document.querySelector(`.navbar__menu`);
+const contactMe = document.getElementById(`home__contact`);
+navbarMenu.addEventListener(`click`, (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+
+  if (link == null) return;
+  const section = document.querySelector(link);
+  window.scrollTo({
+    top: section.offsetTop - navbarHeight + 10,
+    behavior: `smooth`,
+  });
+});
+
+//Handle Contact me button
+contactMe.addEventListener(`click`, () => {
+  const contact = document.querySelector(`#contact`);
+  window.scrollTo({
+    top: contact.offsetTop - navbarHeight + 10,
+    behavior: `smooth`,
+  });
 });
